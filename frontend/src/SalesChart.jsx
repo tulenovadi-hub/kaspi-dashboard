81229d4bad12672e93df694432ab1b2cf2e26436
tulenovadi-hub <tulenovadi@gmail.com>
2026-07-02 03:54:32 +0500

    Update SalesChart.jsx

--- frontend/src/SalesChart.jsx
+++ frontend/src/SalesChart.jsx
@@ -28,7 +28,7 @@ export default function SalesChart({ data, dataKey = 'total_revenue' }) {
   }
 
   return (
-    <ResponsiveContainer width="100%" height={220}>
+    <ResponsiveContainer width="100%" height={300}>
       <AreaChart data={data} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="revenueFill" x1="0" y1="0" x2="0" y2="1">
